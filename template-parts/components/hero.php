3873199faef5f4f98ffd2de5421649268b2d8fc9
Yannick Khalfouni <khalfouni.yannick@gmail.com>
2026-09-02 20:32:32 +0200

    💄 Add neutral base styles for links, buttons and forms

--- template-parts/components/hero.php
+++ template-parts/components/hero.php
@@ -12,28 +12,30 @@
  */
 
 if (!defined('ABSPATH')) {
-    exit;
+    exit();
 }
 
-$title       = $args['title'] ?? '';
-$subtitle    = $args['subtitle'] ?? '';
+$title = $args['title'] ?? '';
+$subtitle = $args['subtitle'] ?? '';
 $button_text = $args['button_text'] ?? '';
-$button_url  = $args['button_url'] ?? '';
-$image       = $args['image'] ?? '';
+$button_url = $args['button_url'] ?? '';
+$image = $args['image'] ?? '';
 ?>
 
-<section class="hero"<?php echo $image ? ' style="background-image: url(' . esc_url($image) . ');"' : ''; ?>>
+<section class="hero"<?php echo $image
+    ? ' style="background-image: url(' . esc_url($image) . ');"'
+    : ''; ?>>
     <div class="hero__content">
-        <?php if ($title) : ?>
+        <?php if ($title): ?>
             <h1 class="hero__title"><?php echo esc_html($title); ?></h1>
         <?php endif; ?>
 
-        <?php if ($subtitle) : ?>
+        <?php if ($subtitle): ?>
             <p class="hero__subtitle"><?php echo esc_html($subtitle); ?></p>
         <?php endif; ?>
 
-        <?php if ($button_text && $button_url) : ?>
-            <a href="<?php echo esc_url($button_url); ?>" class="hero__button">
+        <?php if ($button_text && $button_url): ?>
+            <a href="<?php echo esc_url($button_url); ?>" class="btn hero__button">
                 <?php echo esc_html($button_text); ?>
             </a>
         <?php endif; ?>
